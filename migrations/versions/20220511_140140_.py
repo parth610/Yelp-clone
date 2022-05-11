ba5d33ddbe0616884417ebf1c22cdacdd238a616
Parth Bhakta <bhaktaparthh@gmail.com>
2022-05-11 14:08:29 -0600

    first-commit-startup

--- migrations/versions/20220511_140140_.py
+++ migrations/versions/20220511_140140_.py
@@ -1,8 +1,8 @@
-"""create_users_table
+"""empty message
 
-Revision ID: ffdc0a98111c
+Revision ID: af223df11c66
 Revises: 
-Create Date: 2020-11-20 15:06:02.230689
+Create Date: 2022-05-11 14:01:40.942609
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ffdc0a98111c'
+revision = 'af223df11c66'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -27,7 +27,7 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
-    # ### end Alembic commands ###qqqqqqqqq
+    # ### end Alembic commands ###
 
 
 def downgrade():
