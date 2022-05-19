4b204987122188c24416975627957952caf9484f
Parth Bhakta <bhaktaparthh@gmail.com>
2022-05-19 01:23:11 -0600

    aws-and-some-styling

--- app/api/business_listing_routes.py
+++ app/api/business_listing_routes.py
@@ -4,14 +4,33 @@ from flask_login import login_required, current_user
 from app.models import Business, business, db
 from app.forms import BusinessForm
 from .auth_routes import validation_errors_to_error_messages
+from app.aws_uploads.aws_helper_funcs import (allowed_file, get_unique_filename, upload_file_to_s3)
 
 business_routes = Blueprint('business', __name__)
 
 @business_routes.route('/', methods=['POST'])
 @login_required
 def create_business_listing():
-    business_data = request.get_json(force=True)
+    print('...................')
+    # business_data = request.get_json(force=True)
+    print('--------------------')
     form = BusinessForm()
+
+    files = request.files.getlist('images')
+    print(files, '......................')
+    print(form.data, 'mndawdawkd;////////')
+    photos_dict = {}
+    for i in range(len(files)):
+        if not allowed_file(files[i].filename):
+            return {"errors": "file type not supported"}
+        files[i].filename = get_unique_filename(files[i].filename)
+        print(files[i].filename, '...............//////////////')
+        upload = upload_file_to_s3(files[i])
+        print(upload, '...............//////////////')
+        if "url" not in upload:
+            return upload, 400
+        url = upload["url"]
+        photos_dict[f"{i}"] = url
 
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
@@ -24,7 +43,9 @@ def create_business_listing():
             city = form.data['city'],
             state = form.data['state'],
             zip_code = form.data['zip_code'],
-            business_type = business_data['businessType']
+            business_type = request.form['businessType'],
+            photos = photos_dict
+
         )
         db.session.add(business_listing)
         db.session.commit()
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, NavLink } from 'react-router-dom';
 import { login } from '../../store/session';
+import './LoginForm.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +32,44 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
+    <div className='login-form-container'>
+    <form className='login-form' onSubmit={onLogin}>
+      <div className='login-form-header'>Log in to add reviews or list your business</div>
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
+
         <input
+        className='login-inputs'
           name='email'
           type='text'
           placeholder='Email'
           value={email}
           onChange={updateEmail}
         />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
+
+
         <input
+        className='login-inputs'
           name='password'
           type='password'
           placeholder='Password'
           value={password}
           onChange={updatePassword}
         />
-        <button type='submit'>Login</button>
+        <button className='login-form-button' type='submit'>Login</button>
+        <div>
+        Don't have an account?
       </div>
+      <NavLink className='login-link-signup-form' to='/sign-up' exact={true} activeClassName='active'>
+          <div>
+                Sign Up
+          </div>
+            </NavLink>
     </form>
+    </div>
   );
 };
 
