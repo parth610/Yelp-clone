10f5dfd088a2748bbb0e3ca78fd6b8588dd9f53d
Parth Bhakta <bhaktaparthh@gmail.com>
2022-05-19 15:50:05 -0600

    landing-page

---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'
+
+const FooterComponent = () => {
+    return (
+        <div className="footer-container">
+            <h1>Footer</h1>
+        </div>
+    )
+}
+
+export default FooterComponent;
